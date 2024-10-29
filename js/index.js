54f980eb1d786c04f379b03b54d7557325bb8776
аппле <apple@MacBook-Air-apple.local>
2024-10-29 06:07:55 +0500

    Full

---
+++ js/index.js
@@ -0,0 +1,166 @@
+// 1-masala
+// function changeTheElementsToUppercase(arr) {
+//     let res = arr.map(function(value){
+//         return value.toUpperCase()
+//     })
+//     return res
+// }
+
+// 2-masala
+
+// function iterateStringToTheOtherDirection(str) {
+//     let res = str.split("").reverse().join("")
+//     return res
+// }
+
+// 3-masala
+
+// function theAverageValueOFMassiveNumbers(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i];
+//     }
+//     let res = sum / arr.length;
+//     return res   
+// }
+
+// 4-masala
+
+// function theLengthOfElemnts(arr) {
+//     let res = arr.map(function(value){
+//         return value.length;
+//     })
+//     return res
+// }
+
+// 5-masala
+
+// function onlyOddNumbers(arr) {
+//     let res = arr.filter(function(value){
+//         return value % 2 == 1;
+//     })
+//     return res
+// }
+
+// 6-masala
+
+// function collectTwoObjects(first,second) {
+//     let res = { ...first, ...second};
+//     return res;
+// }
+
+// 7-masala
+
+// function trimElements(str) {
+//     let res = str.map(function(value){
+//         value.trim()
+//     })
+//     return res;
+// }
+
+// 8-masala
+
+// function convertStringIntoArray(str) {
+//     let res = str.split("").map(function(value){
+//         return value;
+//     })
+//     return res;
+// }
+
+// 9-masala
+
+// function findTheBiggestAndSmallest(arr) {
+//     let big = arr[0];
+//     let small = arr[0];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (big < arr[i]) {
+//             big = arr[i];
+//         }
+//     }
+//     if (small > arr[i]) {
+//         small = arr[i];
+//     }
+//     return {big,small}
+// }
+
+// 10-masala
+
+// function howManyVocalicLettersWeHave(str) {
+//     let counter = 0;
+//     for (let i = 0; i < str.length; i++) {
+//         if ("aeiouo".includes(arr)) {
+//             counter++
+//         }
+//         return counter
+//     }
+// }
+
+// 11-masala
+
+// function plusTwoForEveryElement(arr) {
+//     let res = arr.map(function(value){
+//         value = value + 2;
+//     })
+//     return res
+// }
+
+// 12-masala
+
+// function theLongestWord(arr) {
+//     let longest = 0;
+//     theInformation = "";
+//     for (let i = 0; i < arr.length; i++ ) {
+//         if (arr[i].length > longest){
+//             arr[i].length = longest;
+//             theInformation = arr[i];
+//         }
+//     }
+//     return theInformation
+// }
+
+// 13-masala
+
+// function OveraverageValue(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++){
+//         sum =+ arr[i];
+//     }
+//     let average = sum / arr.length
+//     let res = arr.filter(function(value){
+//         return value > average
+//     })
+//     return res
+// }
+
+// 14-masala
+
+// function deleteTheFIrstAndLast(str) {
+//        return str.slice(1,-1);
+// }
+
+// 15-masala
+
+// function theSumOfKvadrat(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++){
+//         sum += arr[i] ** 2
+//     }
+// return sum
+// }
+
+// 16-masala 
+
+// function reverseArray(arr) {
+//     arr.reverse()
+//     return arr
+// }
+
+// 17-masala
+
+// function changeTheLettersToUpperCase(arr) {
+//    for (let i = 0; i < arr.length; i++){
+//     arr[i] = arr[i].toUppercase()
+//    }
+//    return arr;
+// }
+
